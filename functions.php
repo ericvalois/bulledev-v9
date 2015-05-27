6a7d6d7d2a587d3e758ef91f8c914ba184944d19
Eric Valois <eric.v@bulledev.com>
2015-05-27 12:31:34 -0400

    Remove emoji from WP 4.2

--- functions.php
+++ functions.php
@@ -182,6 +182,16 @@ function remove_disqus_count(){
 
 // Some cleanup
 function bulledev_head_cleanup() {
+
+    // Remove emoji from WP 4.2
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );    
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );  
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    
     remove_action( 'wp_head', 'rsd_link' );
     // windows live writer
     remove_action( 'wp_head', 'wlwmanifest_link' );
