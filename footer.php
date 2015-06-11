ee6cb432283cf453c518d2ffe21f0b85fc964918
Eric Valois <eric.v@bulledev.com>
2015-06-11 09:49:46 -0400

    Update GA

--- footer.php
+++ footer.php
@@ -24,6 +24,7 @@
 <?php wp_footer(); ?>
 
 <?php if( strpos($_SERVER['SERVER_NAME'], 'bulledev.com') ): ?>
+	
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -33,6 +34,19 @@
 		ga('create', 'UA-43847997-1', {'siteSpeedSampleRate': 100});
 		ga('send', 'pageview');
 	</script>
+	<?php /* ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-43847997-1', 'auto');
+	  ga('create', 'UA-43847997-1', {'siteSpeedSampleRate': 100});
+	  ga('send', 'pageview');
+
+	</script>
+	<?php */ ?>
 <?php endif; ?>
 	
 </body>
